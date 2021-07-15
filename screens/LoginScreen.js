7a76b8b00c8a11e2fc9bcaf2fc841b887725740f
lklaus <lklaus@umich.edu>
2021-07-15 14:59:17 -0400

    tiny change

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -41,21 +41,41 @@ const LoginScreen = ( {navigation} ) => {
                 placeholder="Enter your password" 
                 onChangeText={password=>setPassword(password)}
             />
+
             <Pressable
+                style = {styles.submit}
                 onPress={handleSubmit}
                 disabled={validLogin}
             >
                 <Text>Submit</Text>
                 
             </Pressable>
+            
             <Pressable
                 onPress={handlePress}
             >
-                <Text>"Don't have an account? Sign up here"</Text>
+                <Text style={{color: 'blue'}}>Don't have an account? Sign up here</Text>
             </Pressable>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    submit: {
+        backgroundColor: '#fff',
+        width: 100,
+        height: 30,
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+        
+        
+    },
+
+    
+})
+
 
 
 export default LoginScreen;
